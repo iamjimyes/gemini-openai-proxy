646d239643614fd8e656ca172ef1129377fe7d19
zuisong <com.me@foxmail.com>
2024-03-14 16:12:15 +0800

    support function_calls

--- src/openai/chat/completions/NonStreamingChatProxyHandler.ts
+++ src/openai/chat/completions/NonStreamingChatProxyHandler.ts
@@ -1,12 +1,25 @@
 import { generateContent } from "../../../gemini-api-client/gemini-api-client.ts"
+import type { FunctionCall } from "../../../gemini-api-client/types.ts"
+import type { Logger } from "../../../log.ts"
 import type { OpenAI } from "../../../types.ts"
 import { type ApiParam, genModel } from "../../../utils.ts"
 
-export async function nonStreamingChatProxyHandler(req: OpenAI.Chat.ChatCompletionCreateParams, apiParam: ApiParam) {
+export async function nonStreamingChatProxyHandler(
+  req: OpenAI.Chat.ChatCompletionCreateParams,
+  apiParam: ApiParam,
+  log?: Logger,
+) {
   const [model, geminiReq] = genModel(req)
-  const geminiResp: string = await generateContent(apiParam, model, geminiReq)
-    .then((it) => it.response.text())
-    .catch((e) => e.message ?? e?.toString())
+  const geminiResp: string | FunctionCall = await generateContent(apiParam, model, geminiReq)
+    .then((it) => it.response.result())
+    .catch((err) => {
+      // 出现异常时打印请求参数和响应，以便调试
+      log?.error(req)
+      log?.error(err?.message ?? err.toString())
+      return err?.message ?? err.toString()
+    })
+  log?.debug(req)
+  log?.debug(geminiResp)
 
   const resp: OpenAI.Chat.ChatCompletion = {
     id: "chatcmpl-abc123",
@@ -15,7 +28,17 @@ export async function nonStreamingChatProxyHandler(req: OpenAI.Chat.ChatCompleti
     model: req.model,
     choices: [
       {
-        message: { role: "assistant", content: geminiResp },
+        message: {
+          role: "assistant",
+          content: typeof geminiResp === "string" ? geminiResp : null,
+          function_call:
+            typeof geminiResp === "string"
+              ? undefined
+              : {
+                  name: geminiResp.name,
+                  arguments: JSON.stringify(geminiResp.args),
+                },
+        },
         logprobs: null,
         finish_reason: "stop",
         index: 0,
